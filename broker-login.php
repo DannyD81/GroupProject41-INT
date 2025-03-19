042b022c87c3cd05bba00cd1ce0e0d61bd2a6c7b
DannyD81 <113707976+DannyD81@users.noreply.github.com>
2025-03-19 16:48:03 +0000

    Add files via upload

---
+++ broker-login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>loginpage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script><div class="home">
+    <link rel="stylesheet" href="style.css">
+</head> 
+<main class="form-signin w-100 m-auto">
+  <form>
+    <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Broker sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    <p>Return to login?<a href="login-page.php"> Click Here</a></p>
+    <p class="mt-5 mb-3 text-body-secondary">© 2024</p>
+  </form>
+</main>
+<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <img class="d-block mx-auto mb-4" src="logo.png" alt="" width="50" height="50">
+        <h3 class="index.php">Rose Brokers</h3> 
+        </a></a>
+      </div>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="index.php" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="calculator.php" class="nav-link px-2">Mortgage Calculator</a></li>
+        <li><a href="about.php" class="nav-link px-2">About</a></li>
+        <li><a href="news.php" class="nav-link px-2">News</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+      <a href="login-page.php">
+      <button type="button" class="btn btn-outline-primary me-2">Login</button>
+      </a>
+      <a href="sign-up-page.php">
+          <button type="button" class="btn btn-primary">Sign-up</button>
+      </a>
+
+      </div>
+    </header>
+  </div>
+  
